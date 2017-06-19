2d37fda514ef8fa23ff8ff9eb37c70af31ce9f37
Jabari Booker <booker.oronde@gmail.com>
2017-06-19 17:49:26 -0400

    Started working on loading audio data into array of samples

--- FFTW Demo/main.cpp
+++ FFTW Demo/main.cpp
@@ -3,13 +3,14 @@
 #include <bitset>
 
 #include <al.h>
+
 #include <alc.h>
 
 #include <fftw3.h>
 
 #include "Sound.h"
 
-float *BytesToFloats(unsigned char* in, float* out, int inSize, int outSize, int bytesPerPoint);
+void BytesToFloats(unsigned char* in, float* out, int inSize, int outSize, int bytesPerPoint);
 
 int main() {
 	ALCdevice* device = alcOpenDevice(NULL);
@@ -21,9 +22,9 @@ int main() {
 
 	Sound music("you've been hit by.wav");
 
-	
+
 	const int sampleSize = music.getBitsPerSample() / 8;
-	const int sampleNum = 1;
+	const int sampleNum = 10;
 	const int dataSize = sampleSize * sampleNum;
 
 	unsigned char* data = (unsigned char*)malloc(dataSize);
@@ -33,6 +34,12 @@ int main() {
 	}
 
 	float *samples = (float*)malloc(sizeof(float) * sampleNum);
+
+	BytesToFloats(data, samples, dataSize, sampleNum, sampleSize);
+
+	for (int i = 0; i < sampleNum; i++) {
+		std::cout << samples[i] << std::endl;
+	}
 
 	fgetc(stdin);
 
@@ -48,6 +55,26 @@ int main() {
 }
 
 
-float *BytesToFloats(unsigned char* in, float* out, int inSize, int outSize, int bytesPerPoint) {
-	return 0;
+void BytesToFloats(unsigned char* in, float* out, int inSize, int outSize, int bytesPerPoint) {
+	for (int i = 0; i < outSize; i++) {
+		unsigned char bytes[4] = { 0,0,0,0 };
+		for (int j = 0; j < bytesPerPoint; j++) {
+			bytes[j] = in[(i*bytesPerPoint) + j];
+		}
+
+		if (bytesPerPoint == 8) {
+			std::bitset<8> x(bytes[2]);
+			std::cout << x << std::endl;
+			if (((bytes[3] >> 7) & 1)) bytes[0] = bytes[1] = bytes[2] = 0xff;
+		}
+		else if (bytesPerPoint == 16) {
+			for(int i=7; i>=0)
+			if (((bytes[2] >> 7) & 1)) bytes[0] = bytes[1] = 0xff;
+		}
+		long bin =	((long)bytes[0])		& 0x000000ff |
+					((long)bytes[1] << 8)	& 0x0000ff00 |
+					((long)bytes[2] << 16)	& 0x00ff0000 |
+					((long)bytes[3] << 24)	& 0xff000000;
+		out[i] = (float)bin;
+	}
 }
